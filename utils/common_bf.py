bf7f054b017d6eb241570a3ae932990b8c8eb26e
sadasilv <sadasilv@redhat.com>
2024-11-06 16:21:29 -0500

    utils/common_bf.py: Use the run function from the common module

--- utils/common_bf.py
+++ utils/common_bf.py
@@ -1,10 +1,9 @@
 import dataclasses
 import os
-import shlex
-import subprocess
 import sys
 import argparse
 from typing import Optional
+from utils.common import run
 
 
 @dataclasses.dataclass(frozen=True)
@@ -12,21 +11,6 @@ class Result:
     out: str
     err: str
     returncode: int
-
-
-def run(cmd: str, env: dict[str, str] = os.environ.copy()) -> Result:
-    args = shlex.split(cmd)
-    res = subprocess.run(
-        args,
-        capture_output=True,
-        env=env,
-    )
-
-    return Result(
-        out=res.stdout.decode("utf-8"),
-        err=res.stderr.decode("utf-8"),
-        returncode=res.returncode,
-    )
 
 
 def all_interfaces() -> dict[str, str]:
